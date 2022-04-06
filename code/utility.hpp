21b77576069a61dc1362efb93c18fccc02e0376e
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2022-04-07 01:57:38 +0200

    reading stupid data

--- code/utility.hpp
+++ code/utility.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <cassert>
-#include <fstream>
-#include <regex>
+#include <cstdio>
+#include <cstring>
 
 template <typename T>
 void swap(T& lhs, T&rhs)
@@ -15,13 +15,41 @@ void swap(T& lhs, T&rhs)
 struct Film
 {
     int Lp;
-    const char* name;
     float rate;
+    char name[256];
+    
+    bool operator<(const Film& rhs) {return rate<rhs.rate;};
+    bool operator>(const Film& rhs) {return rate>rhs.rate;};
+    bool operator<=(const Film& rhs) {return rate<=rhs.rate;};
+    bool operator>=(const Film& rhs) {return rate>=rhs.rate;};
 };
 
 Film* getData(int size)
 {
     Film* films = new Film[size];
-    std::ifstream in("data.csv");
-    assert(in.is_open());
+    FILE* file = fopen("data.csv", "r");
+    assert(file);
+    char line[256];
+    int lp;
+    char* name;
+    float rate;
+    int i = 0;
+    while (fgets(line, 256, file) && i < size)
+    {
+        lp = atoi(strtok(line, ","));
+        name = strtok(NULL, ",");
+        while (name != NULL && name[0] == '\"' && name[strlen(name) - 1] != '\"')
+        {
+            name = strcat(name, strtok(NULL, ","));
+        }
+        rate = atof(strtok(NULL, ","));
+        if (name != NULL && rate != 0)
+        {
+            films[i].Lp = lp;
+            films[i].rate = rate;
+            strcpy(films[i].name, name);
+            ++i;
+        }
+    }
+    return films;
 }
