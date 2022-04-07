c20c45e303a08f983a5133d6f0bf497e27e67f35
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2022-04-07 17:56:13 +0200

    main

--- code/main.cpp
+++ code/main.cpp
@@ -1,13 +1,5 @@
-#include "tests/quicksortTest.hpp"
-#include <iostream>
+
 int main()
 {
-    int datasize = 100000;
-    auto a = getData(datasize);
-    quicksort(a, 0, datasize - 1);
-    for (int i = 0; i < datasize; ++i)
-    {
-        std::cout << a[i].Lp << " " << a[i].name << " " << a[i].rate << std::endl;
-    }
     return 0;
 }
