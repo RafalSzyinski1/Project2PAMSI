cfeb3869c659536116c01a56099efa901e313628
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2022-04-07 17:52:03 +0200

    quicksort Benchmark

--- code/benchmark/quicksortBenchmark.hpp
+++ code/benchmark/quicksortBenchmark.hpp
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "benchmark/benchmark.hpp"
+#include "quicksort.hpp"
+#include "data.hpp"
+
+void bench1()
+{
+    long long data_size = 10000;
+    auto data = getData(data_size);
+    long long time = benchmark([&](){quicksort(data, 0, data_size - 1);});
+    printf("Benchmark 1: Data Size=%lld, Time=%lldms\n", data_size, time);
+}
+
+void bench2()
+{
+    long long data_size = 100000;
+    auto data = getData(data_size);
+    long long time = benchmark([&](){quicksort(data, 0, data_size - 1);});
+    printf("Benchmark 2: Data Size=%lld, Time=%lldms\n", data_size, time);
+}
+
+void bench3()
+{
+    auto data = getData(FILTRED_DATA_SIZE);
+    long long time = benchmark([&](){quicksort(data, 0, FILTRED_DATA_SIZE - 1);});
+    printf("Benchmark 3: Data Size=AllData, Time=%lldms\n", time);
+}
+
+void quicksortBenchmark()
+{
+    bench1();
+    bench2();
+    bench3();
+}
